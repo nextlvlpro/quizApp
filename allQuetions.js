a3f0a9755d05aba4bd8e16581ab2ec9cc0332b1f
nextlvlpro <80281253+nextlvlpro@users.noreply.github.com>
2023-05-30 22:08:51 +0530

    Add files via upload
    quiz app files

---
+++ allQuetions.js
@@ -0,0 +1,475 @@
+var quetions = {
+
+    // Array for  Pipes and Cisterns
+    queCat1: [
+        {
+            quetion:
+                "Three pipes A, B and C can fill a tank from empty to full in 30 minutes, 20 minutes, and 10 minutes respectively. When the tank is empty, all the three pipes are opened. A, B and C discharge chemical solutions P,Q and R respectively. What is the proportion of the solution R in the liquid in the tank after 3 minutes?",
+
+            answers: [
+                { ans: "5/11 th", correct: "false" },
+                { ans: "6/11 th", correct: "true" },
+                { ans: "7/11 th", correct: "false" },
+                { ans: "8/11", correct: "false" },
+            ]
+        },
+
+        {
+            quetion:
+                "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in",
+
+            answers: [
+                { ans: "12hrs", correct: "false" },
+                { ans: "13hrs", correct: "false" },
+                { ans: "14hrs", correct: "false" },
+                { ans: "15hrs", correct: "true" },
+            ],
+        },
+
+        {
+            quetion:
+                "Two pipes A and B can fill a cistern in 37 minutes and 45 minutes respectively. Both pipes are opened. The cistern will be filled in just half an hour, if the B is turned off after",
+
+            answers: [
+                { ans: "5min", correct: "false" },
+                { ans: "9min", correct: "true" },
+                { ans: "10min", correct: "false" },
+                { ans: "15min", correct: "false" },
+            ],
+        },
+
+        {
+            quetion:
+                "A tank is filled by three pipes with uniform flow. The first two pipes operating simultaneously fill the tank in the same time during which the tank is filled by the third pipe alone. The second pipe fills the tank 5 hours faster than the first pipe and 4 hours slower than the third pipe. The time required by the first pipe is",
+
+            answers: [
+                { ans: "6hrs", correct: "false" },
+                { ans: "10hrs", correct: "false" },
+                { ans: "15hrs", correct: "true" },
+                { ans: "18hrs", correct: "false" },
+            ],
+        },
+
+        {
+            quetion:
+                "Two pipes can fill a tank in 20 and 24 minutes respectively and a waste pipe can empty 3 gallons per minute. All the three pipes working together can fill the tank in 15 minutes. The capacity of the tank in gallon",
+
+            answers: [
+                { ans: "60 ", correct: "false" },
+                { ans: "100 ", correct: "false" },
+                { ans: "50", correct: "true" },
+                { ans: "180 ", correct: "false" },
+            ],
+        },
+
+        {
+            quetion:
+                "A tank is filled in 5 hours by three pipes A, B and C. The pipe C is twice as fast as B and B is twice as fast as A. How much time will pipe A alone take to fill the tank?",
+
+            answers: [
+                { ans: "20 hrs", correct: "false" },
+                { ans: "25hrs", correct: "false" },
+                { ans: "30 hrs", correct: "false" },
+                { ans: "35 hrs", correct: "true" },
+            ],
+        },
+
+        {
+            quetion:
+                "Two pipes A and B together can fill a cistern in 4 hours. Had they been opened separately, then B would have taken 6 hours more than A to fill the cistern. How much time will be taken by A to fill the cistern separately?",
+
+            answers: [
+                { ans: " 1 hr", correct: "false" },
+                { ans: "2hr", correct: "false" },
+                { ans: "6 hr", correct: "true" },
+                { ans: "8hrs", correct: "false" },
+            ],
+        },
+
+        {
+            quetion:
+                "Two pipes A and B can fill a tank in 20 and 30 minutes respectively. If both the pipes are used together, then how long will it take to fill the tank?",
+
+            answers: [
+                { ans: " 12 min", correct: "true" },
+                { ans: "15 min", correct: "false" },
+                { ans: "25 min", correct: "false" },
+                { ans: "50 min", correct: "false" },
+            ],
+        },
+
+        {
+            quetion:
+                " A large tanker can be filled by two pipes A and B in 60 minutes and 40 minutes respectively. How many minutes will it take to fill the tanker from empty state if B is used for half the time and A and B fill it together for the other half?",
+
+            answers: [
+                { ans: " 30 min", correct: "true" },
+                { ans: "15 min", correct: "false" },
+                { ans: "25 min", correct: "false" },
+                { ans: "40 min", correct: "false" },
+            ],
+        },
+
+        {
+            quetion:
+                "Three pipes A, B and C can fill a tank in 6 hours. After working at it together for 2 hours, C is closed and A and B can fill the remaining part in 7 hours. The number of hours taken by C alone to fill the tank is",
+
+            answers: [
+                { ans: " 14 hrs", correct: "true" },
+                { ans: "15 hrs", correct: "false" },
+                { ans: "22 hrs", correct: "false" },
+                { ans: "10 hrs", correct: "false" },
+            ],
+        },
+    ],
+
+
+    // Array for  probability
+    queCat2: [
+        {
+            quetion:
+                "Tickets numbered 1 to 20 are mixed up and then a ticket is drawn at random. What is the probability that the ticket drawn has a number which is a multiple of 3 or 5?",
+
+            answers: [
+                { ans: "1/2", correct: "false" },
+                { ans: "2/5", correct: "false" },
+                { ans: "8/15", correct: "false" },
+                { ans: "9/20", correct: "true" },
+            ]
+        },
+
+        {
+            quetion:
+                "A bag contains 2 red, 3 green and 2 blue balls. Two balls are drawn at random. What is the probability that none of the balls drawn is blue?",
+
+            answers: [
+                { ans: "10/21", correct: "true" },
+                { ans: "2/7", correct: "false" },
+                { ans: "5/7", correct: "false" },
+                { ans: "11/21", correct: "false" },
+            ]
+        },
+
+        {
+            quetion:
+                "In a box, there are 8 red, 7 blue and 6 green balls. One ball is picked up randomly. What is the probability that it is neither red nor green?",
+
+            answers: [
+                { ans: "1/3", correct: "true" },
+                { ans: "3/4", correct: "false" },
+                { ans: "4/19", correct: "false" },
+                { ans: "8/21", correct: "false" },
+            ]
+        },
+
+        {
+            quetion:
+                "What is the probability of getting a sum 9 from two throws of a dice?",
+
+            answers: [
+                { ans: "1/9", correct: "true" },
+                { ans: "3/4", correct: "false" },
+                { ans: "4/9", correct: "false" },
+                { ans: "8/3", correct: "false" },
+            ]
+        },
+
+        {
+            quetion:
+                "Three unbiased coins are tossed. What is the probability of getting at most two heads?",
+
+            answers: [
+                { ans: "1/4", correct: "false" },
+                { ans: "3/4", correct: "false" },
+                { ans: "4/9", correct: "false" },
+                { ans: "7/8", correct: "true" },
+            ]
+        },
+
+        {
+            quetion:
+                "Two dice are thrown simultaneously. What is the probability of getting two numbers whose product is even?",
+
+            answers: [
+                { ans: "1/2", correct: "false" },
+                { ans: "3/4", correct: "true" },
+                { ans: "5/9", correct: "false" },
+                { ans: "3/8", correct: "false" },
+            ]
+        },
+
+        {
+            quetion:
+                "In a class, there are 15 boys and 10 girls. Three students are selected at random. The probability that 1 girl and 2 boys are selected, is:",
+
+            answers: [
+                { ans: "21/46", correct: "true" },
+                { ans: "25/117", correct: "false" },
+                { ans: "1/50", correct: "false" },
+                { ans: "3/25", correct: "false" },
+            ]
+        },
+
+        {
+            quetion:
+                "In a lottery, there are 10 prizes and 25 blanks. A lottery is drawn at random. What is the probability of getting a prize?",
+
+            answers: [
+                { ans: "1/10", correct: "false" },
+                { ans: "2/5", correct: "false" },
+                { ans: "2/7", correct: "true" },
+                { ans: "5/7", correct: "false" },
+            ]
+        },
+
+        {
+            quetion:
+                "From a pack of 52 cards, two cards are drawn together at random. What is the probability of both the cards being kings?",
+
+            answers: [
+                { ans: "1/15", correct: "false" },
+                { ans: "25/37", correct: "false" },
+                { ans: "35/256", correct: "false" },
+                { ans: "1/221", correct: "true" },
+            ]
+        },
+
+        {
+            quetion:
+                "Two dice are tossed. The probability that the total score is a prime number is:",
+
+            answers: [
+                { ans: "1/6", correct: "false" },
+                { ans: "5/12", correct: "true" },
+                { ans: "1/2", correct: "false" },
+                { ans: "7/9", correct: "false" },
+            ]
+        }
+
+    ],
+
+
+    //Array for Problems on age
+    queCat3: [
+        {
+            quetion:
+                " Father is aged three times more than his son Ronit. After 8 years, he would be two and a half times of Ronit's age. After further 8 years, how many times would he be of Ronit's age?",
+
+            answers: [
+                { ans: "2 time", correct: "true" },
+                { ans: "3 time", correct: "false" },
+                { ans: "4 time", correct: "false" },
+                { ans: "5 time", correct: "false" },
+            ],
+        },
+
+        {
+            quetion:
+                " The sum of ages of 5 children born at the intervals of 3 years each is 50 years. What is the age of the youngest child?",
+
+            answers: [
+                { ans: "4 yrs", correct: "true" },
+                { ans: "8 yrs", correct: "false" },
+                { ans: "7 yrs", correct: "false" },
+                { ans: "5 yrs", correct: "false" },
+            ],
+        },
+
+        {
+            quetion:
+                " A father said to his son, I was as old as you are at the present at the time of your birth. If the father's age is 38 years now, the son's age five years back was",
+
+            answers: [
+                { ans: "14 yrs", correct: "true" },
+                { ans: "19 yrs", correct: "false" },
+                { ans: "33 yrs", correct: "false" },
+                { ans: "38 yrs", correct: "false" },
+            ],
+        },
+
+        {
+            quetion:
+                " A is two years older than B who is twice as old as C. If the total of the ages of A, B and C be 27, then how old is B?",
+
+            answers: [
+                { ans: "7", correct: "false" },
+                { ans: "8", correct: "false" },
+                { ans: "9", correct: "false" },
+                { ans: "10", correct: "true" },
+            ],
+        },
+
+        {
+            quetion:
+                " Present ages of Sameer and Anand are in the ratio of 5 : 4 respectively. Three years hence, the ratio of their ages will become 11 : 9 respectively. What is Anand's present age in years?",
+
+            answers: [
+                { ans: "24", correct: "true" },
+                { ans: "27", correct: "false" },
+                { ans: "40", correct: "false" },
+                { ans: "15", correct: "false" },
+            ],
+        },
+
+        {
+            quetion:
+                " A man is 24 years older than his son. In two years, his age will be twice the age of his son. The present age of his son is",
+
+            answers: [
+                { ans: "44", correct: "false" },
+                { ans: "18", correct: "false" },
+                { ans: "20", correct: "false" },
+                { ans: "22", correct: "true" },
+            ],
+        },
+
+        {
+            quetion:
+                " Six years ago, the ratio of the ages of Kunal and Sagar was 6 : 5. Four years hence, the ratio of their ages will be 11 : 10. What is Sagar's age at present?",
+
+            answers: [
+                { ans: "16 yrs", correct: "true" },
+                { ans: "18 yrs", correct: "false" },
+                { ans: "20 yrs", correct: "false" },
+                { ans: "22 yrs", correct: "false" },
+            ],
+        },
+
+        {
+            quetion:
+                " The sum of the present ages of a father and his son is 60 years. Six years ago, father's age was five times the age of the son. After 6 years, son's age will be:",
+
+            answers: [
+                { ans: "12 yrs", correct: "false" },
+                { ans: "18 yrs", correct: "false" },
+                { ans: "20 yrs", correct: "true" },
+                { ans: "25 yrs", correct: "false" },
+            ],
+        },
+
+        {
+            quetion:
+                "  At present, the ratio between the ages of Arun and Deepak is 4 : 3. After 6 years, Arun's age will be 26 years. What is the age of Deepak at present ?",
+
+            answers: [
+                { ans: "12 yrs", correct: "false" },
+                { ans: "15yrs", correct: "true" },
+                { ans: "19 yrs", correct: "false" },
+                { ans: "21 yrs", correct: "false" },
+            ],
+        },
+
+        {
+            quetion:
+                " Sachin is younger than Rahul by 7 years. If their ages are in the respective ratio of 7 : 9, how old is Sachin?",
+
+            answers: [
+                { ans: "16 yrs", correct: "false" },
+                { ans: "18 yrs", correct: "false" },
+                { ans: "24 yrs", correct: "false" },
+                { ans: "24.5 yrs", correct: "true" },
+            ],
+        }
+    ],
+
+    // Array for Profit and loss
+    queCat4: [
+        {
+            quetion: "The cost price of 20 articles is the same as the selling price of x articles. If the profit is 25%, then the value of x is:",
+            answers: [
+                { ans: "15", correct: false },
+                { ans: "16", correct: true },
+                { ans: "18", correct: false },
+                { ans: "25", correct: false },
+            ]
+        },
+        {
+            quetion: "In a certain store, the profit is 320% of the cost. If the cost increases by 25% but the selling price remains constant, approximately what percentage of the selling price is the profit?",
+            answers: [
+                { ans: "30%", correct: false },
+                { ans: "70%", correct: true },
+                { ans: "100%", correct: false },
+                { ans: "250%", correct: false },
+            ]
+        },
+        {
+            quetion: "A vendor bought toffees at 6 for a rupee. How many for a rupee must he sell to gain 20%?",
+            answers: [
+                { ans: "3", correct: false },
+                { ans: "4", correct: false },
+                { ans: "5", correct: true },
+                { ans: "6", correct: false },
+            ]
+        },
+        {
+            quetion: `A shopkeeper expects a gain of 22.5% on his cost price. If in a week, his sale was of Rs. 392, what was his profit?`,
+            answers: [
+                { ans: "18.20", correct: false },
+                { ans: "70", correct: false },
+                { ans: "72", correct: true },
+                { ans: "88.25", correct: false },
+
+            ]
+        },
+        {
+            quetion: "A man buys a cycle for Rs. 1400 and sells it at a loss of 15%. What is the selling price of the cycle?",
+            answers: [
+                { ans: "1090", correct: false },
+                { ans: "1160", correct: false },
+                { ans: "1190", correct: true },
+                { ans: "1202", correct: false },
+
+            ]
+        },
+        {
+            quetion: "Sam purchased 20 dozens of toys at the rate of Rs. 375 per dozen. He sold each one of them at the rate of Rs. 33. What was his percentage profit?",
+            answers: [
+                { ans: "3.5", correct: false },
+                { ans: "4.5", correct: false },
+                { ans: "5.6", correct: true },
+                { ans: "6.5", correct: false },
+
+            ]
+        },
+        {
+            quetion: "On selling 17 balls at Rs. 720, there is a loss equal to the cost price of 5 balls. The cost price of a ball is:",
+            answers: [
+                { ans: "45", correct: false },
+                { ans: "50", correct: false },
+                { ans: "55", correct: false },
+                { ans: "60", correct: true },
+
+            ]
+        },
+        {
+            quetion: "When a plot is sold for Rs. 18,700, the owner loses 15%. At what price must that plot be sold in order to gain 15%?",
+            answers: [
+                { ans: "21,000", correct: false },
+                { ans: "22,500", correct: false },
+                { ans: "25,300", correct: true },
+                { ans: "25,800", correct: false },
+
+            ]
+        },
+        {
+            quetion: "A trader mixes 26 kg of rice at Rs. 20 per kg with 30 kg of rice of other variety at Rs. 36 per kg and sells the mixture at Rs. 30 per kg. His profit percent is:",
+            answers: [
+                { ans: "5%", correct: true },
+                { ans: "8%", correct: false },
+                { ans: "10%", correct: false },
+                { ans: "None of These", correct: false },
+
+            ]
+        },
+        {
+            quetion: "A trader sells 85 m of cloth for Rs. 8,925 at the profit of Rs. 15/m of cloth. What is the cost price of 1 m of cloth?",
+            answers: [
+                { ans: "84", correct: false },
+                { ans: "90", correct: true },
+                { ans: "96", correct: false },
+                { ans: "102", correct: false },
+
+            ]
+        },
+    ],
+}
